5ba91751c793d255020be53bfbdeb9cdca4fa50e
github username <github email address>
2019-04-25 12:12:13 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,30 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+
+attr_accessor :name #both getter n setter
+
+@@all = []
+
+def initialize()
+    # @name = name
+    @@all << self
+   #instance variable.The body of the initialize method now does nothing else but assign the value of the local variable name to an instance variable @name
+end
+
+def name
+  @name
+end
+
+#
+#   def name
+#     @name
+#   end
+# end
+# maru = Cat.new
+#
+
+  def meow
+    puts "meow!"
+  end
+
+end
